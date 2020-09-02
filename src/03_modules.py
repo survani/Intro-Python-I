cea803392e99f19484c621ebc3dd44f57a428272
survani <giovanicode@outlook.com>
2020-09-02 12:04:06 -0700

    modules.py is now completed.

--- src/03_modules.py
+++ src/03_modules.py
@@ -4,28 +4,35 @@ which allows you to access many system specific variables and
 methods, and the os module, which gives you access to lower-
 level operating system functionality.
 """
-
 import sys
+import os
+
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
 # YOUR CODE HERE
+print("Arguments:", sys.argv[0])
 
 # Print out the OS platform you're using:
 # YOUR CODE HERE
+print("OS Platform:", sys.platform)
+# Prints out darwin for MAC OS X.
 
 # Print out the version of Python you're using:
 # YOUR CODE HERE
+print("Python Version:", sys.version)
+# Version: 3.8.5
 
-
-import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
 # YOUR CODE HERE
+print("Current Process ID:", os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print("Current Working Directory:", os.getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+print("Machines Login Name:", os.getlogin())
