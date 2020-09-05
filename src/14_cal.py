dc9c2358d424395bf5b61ded6816cb37ffab2ec3
survani <giovanicode@outlook.com>
2020-09-04 17:50:08 -0700

    cal.py is now completed.

--- src/14_cal.py
+++ src/14_cal.py
@@ -27,6 +27,51 @@ print out a calendar for April in 2015, but if you omit either the year or both 
 it should use today’s date to get the month and year.
 """
 
+# resouces used to complete this
+
+# sys.argv: https://stackoverflow.com/questions/4117530/sys-argv1-meaning-in-script
+# also https://www.pythonforbeginners.com/system/python-sys-argv
+
+# int(): https://www.programiz.com/python-programming/methods/built-in/int
+
+# == or is usage below:
+# https://www.geeksforgeeks.org/difference-operator-python/#:~:text=The%20%3D%3D%20operator%20compares%20the,the%20same%20object%20or%20not.&text=We%20can%20check%20it%20with,%E2%80%9Cidentity%E2%80%9D%20of%20an%20object.
+
+
 import sys
 import calendar
 from datetime import datetime
+
+# 1st Step
+amnt_args = len(sys.argv)
+
+# 2nd Step - No input specified
+if amnt_args == 1:
+    mm = datetime.now().month
+    yy = datetime.now().year
+
+# 3rd Step - User inputs 1 argument
+    # we are assuming that the user input was for the month, but the year will continue to output datetime.now().year.
+elif amnt_args == 2:
+    # The sys.argv[1] looks into the first argument.
+    # "The int() method returns an integer object from any number or string." - programiz.com
+    mm = int(sys.argv[1])
+    yy = datetime.now().year
+
+# 4th Step - User inputs 2 arguments
+elif amnt_args == 3:
+    # The sys.argv[1] looks into the first argument.
+    # The sys.argv[2] looks into the second argument.
+    mm = int(sys.argv[1])
+    yy = int(sys.argv[2])
+
+
+else:
+    print("How to use: [Month] [Year]")
+
+    # "sys.exit() is considered good to be used in production code for the sys module is always available."
+    # geeksforgeeks.org
+    sys.exit(0)
+
+new_calendar = calendar.TextCalendar()
+new_calendar.prmonth(yy, mm)
